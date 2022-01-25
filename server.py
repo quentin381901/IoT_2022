9c51f4e52a1c2f3500f273081588aac89da3c8ef
Quentin <quentin.comelli@live.fr>
2022-01-25 13:44:37 +0100

    maj

--- server.py
+++ server.py
@@ -1,26 +1,29 @@
 import webbrowser, os, sys, time
-import sqlite3 as sql
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import os, time
-from pigpio_dht import DHT11, DHT22
-os.system('sudo pigpiod')
+import time
 
-class RequestHandler_httpd(BaseHTTPRequestHandler):
-  def do_GET(self):
-    messagetosend = bytes('Hello World',"utf")
-    self.send_response(200)
-    self.send_header('Content-Type', 'text/plain')
-    self.send_header('Content-Length', len(messagetosend))
-    self.end_headers()
-    self.wfile.write(messagetosend)
-    print(self.requestline)
-    return
+hostName = "localhost"
+serverPort = 8080
 
-def open_chromium(url):
-  chrome_path = '/usr/bin/chromium-browser --start-fullscreen'
-  webbrowser.get(chrome_path).open(url)
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(00)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
 
-server_address_httpd = ('localhost',8080)
-httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
-print('Server started')
-httpd.serve_forever()
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    webServer.server_close()
+    print("Server stopped.")
