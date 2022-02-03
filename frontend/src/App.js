7d98d48c8402c01c4511ffff13ef0f210d423988
quentin381901 <q.comelli@it-students.fr>
2022-02-03 15:08:04 +0100

    Parametre nombre de jour a afficher

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,3 @@
-//import React, { useState, useEffect } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-//
-//function App() {
-//  const [currentTest, setTest] = useState(0);
-//
-//  useEffect(() => {
-// /*   fetch('/time')
-//    .then(res => res.json()).then(data => {
-//      setCurrentTime(data.time);
-//    });*//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//*
-//     fetch('/test2')
-//    .then(res => res.json()).then(data => {
-//    console.log("data",data)
-//      setTest(data.name);
-//    });
-//  }, []);
-//
-//  return (
-//    <div className="App">
-//      <header className="App-header">
-//
-//        ... no changes in this part ...
-//
-//        <p>my name is {currentTest}.</p>
-//      </header>
-//    </div>
-//  );
-//}
-//
-//export default App;*/
-/*import React from 'react';
-import './App.css';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import LoginHooks from './components/LoginHooks';
-import LogoutHooks from './components/LogoutHooks';
-import Mirror from './components/Mirror';
-import {BrowserRouter as Router, Route, Routes,Link, Navigate } from 'react-router-dom';
-
-function App() {
-  return (
-  <Router>
-    <div className="App">
-        <h1>mon titre </h1>
-        <div >
-            <Routes>
-                <Route path='/' element={<LoginHooks />}/>
-                <Route path='/logout'  element={<LogoutHooks/>}/>
-                <Route path='/mirror'  element={<Mirror/>}/>
-            </Routes>
-        </div>
-    </div>
-  </Router>
-
-
-  );
-
-}
-
-export default App;*/
 import React from 'react';
 import './App.css';
 import BingNews from './components/BingNews';
@@ -104,8 +42,8 @@ function App() {
       <Scheduler data={sampleData} defaultDate={displayDate}>
       <AgendaView
         title="Compact View"
-        step={2}
-        numberOfDays={2}
+        step={1}
+        numberOfDays={1}
         selectedDateFormat={"From: {0:D} To: {1:D}"}
         selectedShortDateFormat={"From: {0:d} To: {1:d}"}
       />
