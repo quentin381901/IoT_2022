89d87e022658d9fa129641133aa7c8aeaad3c07b
quentin381901 <q.comelli@it-students.fr>
2022-02-03 15:48:14 +0100

    Calandar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import BingNews from './components/BingNews';
 import Login from './components/Login';
@@ -8,29 +8,29 @@ import LogoutHooks from './components/LogoutHooks';
 import Mirror from './components/Mirror';
 
 
-import {BrowserRouter as Router, Route, Routes,Link, Navigate } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link, Navigate} from 'react-router-dom';
+
 function App() {
-  
+
     // let timer = new Date().toLocaleTimeString()
     // console.log("timer",timer)
     // function tick() {return this.timer }
     // setInterval(tick, 1000);
     // console.log("je passe apres ")
     // <h2>It is {timer}</h2>
-  return (
-  <Router>
-    <div className="App">
-      <div id="root">
- 
-      </div>
-        <h1>mon titre </h1>
-        <div >
-            <Routes>
-                <Route path='/' element={<LoginHooks />}/>
-                <Route path='/mirror'  element={<Mirror/>}/>
-            </Routes>
-        </div>
-    </div>
-  </Router>)
-} 
+    return (
+        <Router>
+            <div className="App">
+                <div id="root">
+                </div>
+                <div>
+                    <Routes>
+                        <Route path='/' element={<LoginHooks/>}/>
+                        <Route path='/mirror' element={<Mirror/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </Router>)
+}
+
 export default App;
--- frontend/src/components/Mirror.js
+++ frontend/src/components/Mirror.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import LogoutHooks from './LogoutHooks';
 import BingNews from './BingNews';
+import "@progress/kendo-theme-default/dist/all.css";
+import {Scheduler, AgendaView} from "@progress/kendo-react-scheduler";
+import {sampleData, displayDate} from "../events-utc";
 
 const Mirror = () => {
-    return <div> 
-        <h1> Mirror</h1>  
-    <div><LogoutHooks></LogoutHooks></div> 
-    <div> <BingNews /></div>  
+    return <div>
+        <div><LogoutHooks></LogoutHooks></div>
+        <div><BingNews/></div>
+        <div>
+            <Scheduler data={sampleData} defaultDate={displayDate}>
+                <AgendaView
+                    title="Compact View"
+                    step={1}
+                    numberOfDays={1}
+                    selectedDateFormat={"From: {0:D} To: {1:D}"}
+                    selectedShortDateFormat={"From: {0:d} To: {1:d}"}
+                />
+            </Scheduler>
+        </div>
     </div>
+
 }
 export default Mirror;
